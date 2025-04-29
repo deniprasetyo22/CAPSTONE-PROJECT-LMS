d0d6d10453ce3bf4319a29a55e550957af47084e
hidayattresnadi <hidayattresnadi64@gmail.com>
2025-04-29 19:03:56 +0700

    Add Discussion Feature at student Panel

--- app/Controllers/CourseController.php
+++ app/Controllers/CourseController.php
@@ -24,11 +24,8 @@ class CourseController extends BaseController
     protected EnrollmentModel $enrollmentModel;
     protected $courseContentModel;
     protected $fileModel;
-<<<<<<< HEAD
-    protected $assignmentModel;
-=======
     protected $discussionModel;
->>>>>>> 62a83e676fc19c9fb06cdd19421444f30952813c
+    protected $assignmentModel;
 
     public function __construct()
     {
@@ -39,11 +36,8 @@ class CourseController extends BaseController
         $this->enrollmentModel = new EnrollmentModel();
         $this->courseContentModel = new CourseContentModel();
         $this->fileModel = new FileModel();
-<<<<<<< HEAD
-        $this->assignmentModel = new AssignmentModel();
-=======
         $this->discussionModel = new DiscussionModel();
->>>>>>> 62a83e676fc19c9fb06cdd19421444f30952813c
+        $this->assignmentModel = new AssignmentModel();
     }
 
     public function index(): string
@@ -253,13 +247,19 @@ class CourseController extends BaseController
         $courseContents = $this->courseContentModel->where('course_id', $id)->findAll();
 
         $assignments = $this->assignmentModel->where('course_id', $id)->findAll();
+        $discussions = $this->discussionModel
+            ->where('course_id', $id)
+            ->where('deleted_at', null)
+            ->orderBy('id', 'desc')
+            ->findAll();
 
         $data = [
             'page_title' => $course->name,
             'course' => $course,
             'courseContents' => $courseContents,
             'enrollment' => $enrollment,
-            'assignments' => $assignments
+            'assignments' => $assignments,
+            'discussions' => $discussions,
         ];
 
         return view('pages/student/courses/v_show', $data);
@@ -325,6 +325,8 @@ class CourseController extends BaseController
             ->orderBy('id', 'desc')
             ->findAll();
 
+        $assignments = $this->assignmentModel->where('course_id', $id)->findAll();
+
         $students = $this->enrollmentModel
             ->select('user_profiles.*, users.email, enrollments.id')
             ->join('user_profiles', 'user_profiles.id = enrollments.student_id')
@@ -340,29 +342,22 @@ class CourseController extends BaseController
             ->where('courses_lecturers.deleted_at', null)
             ->findAll();
 
-<<<<<<< HEAD
-        $assignments = $this->assignmentModel->where('course_id', $id)->findAll();
-=======
         $discussions = $this->discussionModel
             ->where('course_id', $id)
             ->where('deleted_at', null)
             ->orderBy('id', 'desc')
             ->findAll();
->>>>>>> 62a83e676fc19c9fb06cdd19421444f30952813c
 
         return view('pages/admin/courses/detail_course', [
             'course' => $course,
             'courseContents' => $courseContents,
             'students' => $students,
             'lecturers' => $lecturers,
-<<<<<<< HEAD
-            'assignments' => $assignments,
-=======
             'discussions' => $discussions,
->>>>>>> 62a83e676fc19c9fb06cdd19421444f30952813c
             'params' => $params,
             'users' => $users['user_profiles'],
             'page_title' => 'Course Detail',
+            'assignments' => $assignments,
             'hideHeader' => true
         ]);
     }
