542cedd8953636784f1c961b79aff4765b5941d3
Deni Prasetyo <deni.prasetyo@solecode.id>
2025-07-09 17:22:46 +0700

    Update Deploy

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -21,7 +21,7 @@ class UserSeeder extends Seeder
 
         $this->db->table('auth_groups_users')->insert([
             'group_id' => 1,
-            'user_id'  => 2
+            'user_id'  => 1
         ]);
 
         $this->db->table('auth_users_permissions')->insert([
